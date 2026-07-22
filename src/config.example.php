d9a2f52589da478532091e22940369713a75192e
Warre van der Maat <60901879+warrevdm@users.noreply.github.com>
2026-07-22 13:46:43 +0200

    Voeg SMTP-configuratievoorbeeld toe

---
+++ src/config.example.php
@@ -0,0 +1,14 @@
+<?php
+// Kopieer dit bestand naar config.php en vul de correcte SMTP-gegevens in.
+
+const SMTP_HOST = 'smtp.office365.com';
+const SMTP_PORT = 587;
+const SMTP_USERNAME = 'verkochtefietsen@aertsactionbike.be';
+const SMTP_PASSWORD = 'VUL_HIER_HET_WACHTWOORD_OF_APP_PASSWORD_IN';
+const SMTP_ENCRYPTION = 'tls';
+
+const MAIL_FROM_ADDRESS = 'verkochtefietsen@aertsactionbike.be';
+const MAIL_FROM_NAME = 'Aerts Action Bike';
+const MAIL_REPLY_TO = 'verkochtefietsen@aertsactionbike.be';
+
+const BOOKING_URL = 'https://outlook.office365.com/book/Verkochtefietsen@aertsactionbike.be/?ismsaljsauthenabled=true';
