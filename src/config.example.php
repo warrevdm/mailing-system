49a805d791069c24de14dcd84a56d47266c397d5
Warre van der Maat <60901879+warrevdm@users.noreply.github.com>
2026-07-22 14:35:23 +0200

    Voeg afzendergegevens toe voor Outlook-concept

--- src/config.example.php
+++ src/config.example.php
@@ -1,4 +1,8 @@
 <?php
 
+// Afzender die in het Outlook-concept wordt ingevuld.
+const MAIL_FROM_ADDRESS = 'verkoop@aertsactionbike.be';
+const MAIL_FROM_NAME = 'Aerts Action Bike';
+
 // Link waarmee klanten een afhaalmoment voor hun verkochte fiets boeken.
 const BOOKING_URL = 'https://outlook.office365.com/book/Verkochtefietsen@aertsactionbike.be/?ismsaljsauthenabled=true';
